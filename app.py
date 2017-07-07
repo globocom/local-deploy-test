eef0a812a6f02e121c521f441aa0ad7e0b692e75
Diogo Munaro <diogo.munaro@corp.globo.com>
2017-07-06 21:57:15 -0300

    Using debugger

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def show_text():
     return os.environ.get('TEXTO', 'Hello World!')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
